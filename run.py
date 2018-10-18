7b4b7dbb3410b4984a17df548b9385045c922d89
denis19973 <oxingteam@gmail.com>
2018-10-18 10:26:17 +0300

    simple neural network without vectorization

--- run.py
+++ run.py
@@ -1,48 +1,91 @@
 import numpy as np
 
-def sigmoid(z):
-	return 1 / (1 + np.exp(-z))
+def sigmoid(x):
+	return 1 / (1 + np.exp(-x))
 
-def sigmoid_derivative(z)
-	return z * (1 - z)
+def sigmoid_deriv(x):
+	return x * (1 - x)
 
-# todo try on old examples(with predicting by first value)
+def initialize_network(n_inputs, n_hidden, n_outputs):
+	network = []
+	hidden_layer = [{'weights': [np.random.rand() for _ in range(n_inputs + 1)]} for _ in range(n_hidden)]
+	network.append(hidden_layer)
+	output_layer = [{'weights': [np.random.rand() for _ in range(n_hidden + 1)]} for _ in range(n_outputs)]
+	network.append(output_layer)
+	return network
 
-# neural network with 3 input units
-class NeuralNetwork:
-	def __init__(self, x, y):
-		self.input = x
-		# todo resolve in future
-		# self.weights1   = np.random.rand(self.input.shape[1],4) 
-		# todo add biases
-		self.weights_1 = np.random.rand(4, 4)
-		self.weights_2 = np.random.rand(1, 5)
-		self.y = y
-		self.output = np.zeros(y.shape)
+def activate(weights, inputs):
+	"""Calculate activation for single neuron"""
+	activation = weights[-1]  # weights[-1] it's bias
+	for i in range(len(weights) - 1):
+		activation += weights[i] * inputs[i]
+	return activation
+
+def forward_propagate(network, row):
+	inputs = row
+	for layer in network:
+		new_inputs = []
+		for neuron in layer:
+			activation = activate(neuron['weights'], inputs)
+			neuron['output'] = sigmoid(activation)
+			new_inputs.append(neuron['output'])
+		inputs = new_inputs
+	return inputs
+
+def backward_propagate_error(network, expected):
+	"""Backpropagate error and store in neurons"""
+	for i in reversed(range(len(network))):
+		layer = network[i]
+		errors = []
+		if i != len(network) - 1:
+			for j in range(len(layer)):
+				error = 0
+				for neuron in network[i + 1]:
+					error += neuron['weights'][j] * neuron['delta']
+				errors.append(error)
+		else:
+			for j in range(len(layer)):
+				neuron = layer[j]
+				errors.append(expected[j] - neuron['output'])
+		for j in range(len(layer)):
+			neuron = layer[j]
+			neuron['delta'] = errors[j] * sigmoid_deriv(neuron['output'])
+
+def update_weights(network, row, l_rate):
+	for i in range(len(network)):
+		inputs = row[:-1]
+		if i != 0:
+			inputs = [neuron['output'] for neuron in network[i - 1]]
+		for neuron in network[i]	:
+			for j in range(len(inputs)):
+				neuron['weights'][j] += l_rate * neuron['delta'] * inputs[j]
+			neuron['weights'][-1] += l_rate * neuron['delta']
+
+def train_network(network, train, l_rate, n_epoch, n_outputs):
+	for epoch in range(n_epoch):
+		sum_error = 0
+		for row in train:
+			outputs = forward_propagate(network, row)
+			expected = [0 for i in range(n_outputs)]
+			expected[row[-1]] = 1
+			sum_error += sum([(expected[i] - outputs[i])**2 for i in range(len(expected))])
+			backward_propagate_error(network, expected)
+			update_weights(network, row, l_rate)
+		print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
 
 
-	def forward_propagate(self):
-		input_rows_count = len(self.input_with_biases)
-		ones = np.ones((input_rows_count,1), float)
-		input_with_biases = np.concatenate((ones, a), axis=1)
-		for row in range(input_rows_count):
-			# activation for 1-st layer it's just input row
-			# with added bias unit
-			activation_1 = input_with_biases[row, :]
-			# activation for 2-nd layer
-			z_2 = np.dot(self.weights_1, activation_1.T) # (4, 1)
-			activation_2 = sigmoid(z)
-			# adding bias
-			activation_2 = np.concatenate((np.ones((1,1), float), activation_2), axis=0) # (5, 1)
-			# output in 3-rd layer
-			z_3 = np.dot(self.weights_2, activation_2)
-			output = sigmoid(z)
-			self.output[row] = output
 
-	def back_propagate(self)
-	
-
-
-
-
-
+dataset = [[2.7810836,2.550537003,0],
+	[1.465489372,2.362125076,0],
+	[3.396561688,4.400293529,0],
+	[1.38807019,1.850220317,0],
+	[3.06407232,3.005305973,0],
+	[7.627531214,2.759262235,1],
+	[5.332441248,2.088626775,1],
+	[6.922596716,1.77106367,1],
+	[8.675418651,-0.242068655,1],
+	[7.673756466,3.508563011,1]]
+n_inputs = len(dataset[0]) - 1
+n_outputs = len(set([row[-1] for row in dataset]))
+network = initialize_network(n_inputs, 2, n_outputs)
+train_network(network, dataset, 0.5, 8000, n_outputs)
